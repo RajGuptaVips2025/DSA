1fe432e18599bdbc939703212a977205e85cb152
RajGuptaVips2025 <rajguptavips2025@gmail.com>
2025-08-09 02:51:02 +0530

    linked list(9aug, 2025)

--- DSA/LinkedList/Q1.cpp
+++ DSA/LinkedList/Q1.cpp
@@ -6,7 +6,7 @@ using namespace std;
 struct Node{
     int data;
     struct Node *next;
-}*first = NULL; // 'first' is a global pointer to the head of the list, initially NULL
+}*first = NULL, *second = NULL, *third = NULL; // 'first' is a global pointer to the head of the list, initially NULL
 
 void create(int A[], int n){
     int i;
@@ -26,6 +26,23 @@ void create(int A[], int n){
         last->next = t; // Link previous node to new node
         last = t;   // Move 'last' to new node
     }
+}
+
+void create2(int A[], int n){
+    int i;
+    struct Node *t, *last;
+    second=(struct Node *)malloc(sizeof(struct Node));
+    second->data=A[0];
+    second->next=NULL;
+    last=second;
+
+    for (int i = 1; i < n; i++){
+        t=(struct Node*)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next=NULL;
+        last->next = t;
+        last=t;
+    }  
 }
 
 void Display(struct Node *p){
@@ -231,16 +248,137 @@ void RemoveDuplicate(struct Node *p){
         }
         
     }
-    
+}
+
+void Reverse1(struct Node *p){
+    int *A, i=0;
+    struct Node *q=p;
+
+    A=(int *)malloc(sizeof(int)*count(p));
+
+    while (q!=NULL){
+        A[i] = q->data;
+        q=q->next;
+        i++;
+    }
+    q=p;
+    // At this point, i is one past the last valid index because the last increment happened before exiting the first loop.
+    // So, i-- moves it back to the index of the last element in the array.
+    i--;
+    while (q!=NULL){
+        q->data = A[i];
+        q=q->next;
+        i--;
+    }
+}
+
+void Reverse2(struct Node *p){
+    struct Node *q=NULL, *r=NULL;
+
+    while(p!=NULL){
+        r=q;
+        q=p;
+        p=p->next;
+        q->next=r;
+    }
+    first=q;
+}
+
+void Reverse3(struct Node *q, struct Node *p){
+    if(p){
+        Reverse3(p, p->next);
+        p->next=q;
+    }else{
+        first=q;
+    }
+}
+
+void Concat(struct Node *p, struct Node *q){
+    third = p;
+
+    while (p->next!=NULL){
+        p=p->next;
+    }
+    p->next=q;    
+}
+
+void Merge(struct Node *p, struct Node *q){
+    struct Node *last;
+    if (p->data <q->data){
+        third = last = p;
+        p=p->next;
+        third->next=NULL;
+    }
+    else{
+        third=last=q;
+        p=p->next;
+        third->next=NULL;
+    }
+    while (p && q){
+        if (p->data < q->data){
+            last->next = p;
+            last = p;
+            p=p->next;
+            last->next=NULL;
+        }
+        else{
+            last->next = q;
+            last=q;
+            q=q->next;
+            last->next=NULL;
+        }       
+    }
+    if (p)last->next=p;
+    if (q)last->next=q;
+}
+
+int isLoop(struct Node *f){
+    struct Node *p, *q;
+    p=q=f;
+
+    do
+    {
+        p=p->next;
+        q=q->next;
+        q=q?q->next:q;
+    } while (p && q && p != q);
+
+    if (p==q)
+        return 1;
+    else
+        return 0;    
 }
 
 int main() {
-    int A[] = {3, 5, 5, 7, 12, 50, 20, 20, 40};
-    create(A, 9);
-    
-    RemoveDuplicate(first);
+    int A[] = {3, 5, 5, 7, 12, 50, 20, 20, 40, 70};
+    struct Node *t1, *t2;
+    int n = sizeof(A) / sizeof(A[0]); // 10
+    create(A, n);
 
-    Display(first);
-    printf("\n\n");
+    t1=first->next->next;
+    t2=first->next->next->next->next;
+    t2->next=t1;
+
+    printf("%d\n", isLoop(first));
+
+
+
+    // int B[] = {1, 223, 333, 424, 5};
+    // create2(B, 5);
+    
+    // RemoveDuplicate(first);
+    // Reverse2(first);
+    // printf("First\n");
+    // Display(first);
+    // printf("\n\n");
+
+    // printf("Second\n");
+    // Display(second);
+    // printf("\n\n");
+
+    // Merge(second,first);
+    // printf("Merged\n");
+    // Display(third);
+    // printf("\n\n");
     return 0;
 }
